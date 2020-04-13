511eb048eb827ef3786e3dc7675ffb46fae74977
Louis Lin <lin.louis@pm.me>
2020-04-13 21:35:17 +0200

    chore: change indentation on front files

--- heart-rate-consumer/src/main/resources/static/assets/heart-rates.stream.js
+++ heart-rate-consumer/src/main/resources/static/assets/heart-rates.stream.js
@@ -1,44 +1,44 @@
 $(function() {
-	const totalPoints = 120;
-	const updateInterval = 1000;
+  const totalPoints = 120;
+  const updateInterval = 1000;
   const buildEventUrl = (userId) => '/users/' + userId + '/heart-rates/stream';
 
   // GLOBAL VARIABLES :scream: --------------------------------------------------------------------
-	let data = [];
+  let data = [];
   let heartRate = null;
 
   // PLOT -----------------------------------------------------------------------------------------
-	const buildData = () => {
-		if (data.length > 0) {
-			data = data.slice(1);
-		}
+  const buildData = () => {
+    if (data.length > 0) {
+      data = data.slice(1);
+    }
 
     if (heartRate && !isNaN(heartRate.value)) {
         data.push(heartRate.value);
     }
-		while (data.length < totalPoints) {
-			data.push(0);
-		}
+    while (data.length < totalPoints) {
+      data.push(0);
+    }
 
-		let res = [];
-		for (let i = 0; i < data.length; ++i) {
-			res.push([i, data[i]])
-		}
-		return res;
-	}
+    let res = [];
+    for (let i = 0; i < data.length; ++i) {
+      res.push([i, data[i]])
+    }
+    return res;
+  }
 
-	let plot = $.plot('.heart-rate-placeholder', [ buildData() ], {
-		series: {
-      shadowSize: 0	// Drawing is faster without shadows
-		},
-		yaxis: {
-			min: 0,
-			max: 250
-		},
-		xaxis: {
-			show: false
-		}
-	});
+  let plot = $.plot('.heart-rate-placeholder', [ buildData() ], {
+    series: {
+      shadowSize: 0  // Drawing is faster without shadows
+    },
+    yaxis: {
+      min: 0,
+      max: 250
+    },
+    xaxis: {
+      show: false
+    }
+  });
 
   // EVENT SOURCE ---------------------------------------------------------------------------------
   const getUserId = () => {
@@ -62,19 +62,19 @@ $(function() {
   };
 
   // INIT -----------------------------------------------------------------------------------------
-	const update = () => {
-		plot.setData([buildData()]);
-		plot.draw();
-		setTimeout(update, updateInterval);
-	};
+  const update = () => {
+    plot.setData([buildData()]);
+    plot.draw();
+    setTimeout(update, updateInterval);
+  };
   const stop = () => {
     console.info('Stopping event...');
     source.close();
   };
 
   // START ----------------------------------------------------------------------------------------
-	plot.setupGrid();
-	update();
+  plot.setupGrid();
+  update();
 
   window.onbeforeunload = () => {
     stop();
